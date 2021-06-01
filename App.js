c6f30f82637778165995f363d442a5185d596f62
mbnaidu <madhucharliehash@gmail.com>
2021-06-01 13:40:53 +0530

    chat page completed

--- App.js
+++ App.js
@@ -1,21 +1,79 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Container, Header, Title, Button, Left, Right, Body, Icon, Tabs, Tab, TabHeading, Badge } from 'native-base';
+import { AppState, StatusBar,Text } from 'react-native';
+import styles from './Styles';
+import Calls from './Screens/Calls';
+import Chats from './Screens/Chats';
+import Status from './Screens/Status';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+export default class App extends Component {
+	constructor(props) {
+		super(props)
+		setTimeout(()=>{
+			StatusBar.setBackgroundColor('#128C7E',true);
+		},100)
+	}
+	render() {
+		return (
+		<Container>
+			<Header noLeft style={styles.headerBackgroundColor}>
+				<Body>
+					<Title style={styles.appTitle}>WhatsApp</Title>
+				</Body>
+				<Right>
+					<Button transparent>
+						<Icon type="MaterialIcons" name="search"/>
+					</Button>
+					<Button transparent>
+						<Icon type="MaterialIcons" name="more-vert"/>
+					</Button>
+				</Right>
+			</Header>
+			<Tabs initialPage={1}
+				tabBarBackgroundColor="#075E54"
+				tabContainerStyle={{elevation:0}}
+				tabBarUnderlineStyle={styles.tabBarUnderline}
+			>
+				<Tab 
+					heading={
+						<TabHeading style={{backgroundColor:"#075E54"}}>
+							<Icon name="photo-camera" type="MaterialIcons" style={{color:"snow"}}/>
+						</TabHeading>}>
+					<Text style={{
+						textAlign:"center",
+						textAlignVertical:"center",
+						flex:1
+						}}>
+						Camera
+					</Text>
+				</Tab>
+				<Tab 
+					heading={
+						<TabHeading style={{backgroundColor:"#075E54"}}>
+							<Text style={styles.textColor}>CHATS </Text>
+							<Badge style={styles.badge}>
+								<Text style={styles.badgeText}>2</Text>
+							</Badge>
+						</TabHeading>}>
+					<Chats/>
+				</Tab>
+				<Tab 
+					heading={
+						<TabHeading style={{backgroundColor:"#075E54"}}>
+							<Text style={styles.textColor}>STATUS</Text>
+						</TabHeading>}>
+						<Status/>
+				</Tab>
+				<Tab 
+					heading={
+						<TabHeading style={{backgroundColor:"#075E54"}}>
+							<Text style={styles.textColor}>CALLS</Text>
+						</TabHeading>}>
+						<Calls/>
+				</Tab>
+			</Tabs>
+		</Container>
+		);
+	}
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ Styles.js
@@ -0,0 +1,35 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+    // appbar
+    appTitle:{
+        fontSize:22,
+        color:"snow"
+    },
+    headerBackgroundColor:{
+        backgroundColor:"#075E54"
+    },
+    tabBarUnderline:{
+        height:2
+    },
+    // tabColorText
+    textColor:{
+        fontWeight:"bold",
+        color:"snow",
+        fontSize:15,
+    },
+    // badge
+    badge: {
+        height:24,
+        width:24,
+        backgroundColor:"#ECE5DD",
+        justifyContent:"center",
+        alignSelf:"center",
+        textAlign:"center"
+    },
+    badgeText:{
+        alignSelf:"center",
+        fontSize:12,
+        color:"#075E54"
+    }
+})
