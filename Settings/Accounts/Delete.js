08be71ce1392d9ff9625da6ec192a17db7251158
mbnaidu <madhucharliehash@gmail.com>
2021-06-05 16:42:45 +0530

    Update Delete.js

--- Settings/Accounts/Delete.js
+++ Settings/Accounts/Delete.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Container, Header, Left, Body, Right, Button, Icon, Title, Thumbnail, Footer, Item, Input, Content, Fab, View, ListItem, Badge, Switch, Card, CardItem, Picker, DatePicker, Label } from 'native-base';
 import { StatusBar, Text, TextInput,Image, Keyboard, Modal,Pressable, TouchableWithoutFeedback, ScrollView } from 'react-native';
+import styles from '../../Styles/First';
 
 export default class Delete extends Component {
 	render() {
@@ -14,57 +15,71 @@ export default class Delete extends Component {
 							<Icon name='arrow-back' type="MaterialIcons" style={{fontSize: 28}}/>
 						</Button>
 					</Left>
-					<Body>
-                        <Title>Delete</Title>
+					<Body style={{marginRight:150}}>
+                        <Title>Delete my account</Title>
                     </Body>
-					<Right>
-						<Button transparent>
-							<Icon name='more-vert' type="MaterialIcons" style={{fontSize: 0}}/>
-						</Button>
-					</Right>
 				</Header>
 					<ScrollView>
                     <ListItem avatar noBorder button onPress={() =>{navigate('Accounts')}}>
                         <Left>
                             <Button transparent onPress={()=>{navigate('Chat')}}>
-                                <Icon name='security' type="MaterialIcons" style={{fontSize: 28,color:"#075E54"}}/>
+                                <Icon name='alert' type="Foundation" style={{fontSize: 28,color:"red"}}/>
                             </Button>
                         </Left>
-                        <Body style={{marginTop:10}}>
-                            <Text style={{fontSize:18}}>Privacy</Text>
+                        <Body>
+                            <View>
+                                <View>
+                                    <Text style={{fontSize:18}}>Deleting your account will:</Text>
+                                </View>
+                                <View style={{marginTop:10}}>
+                                    <Text style={{fontSize:15}}>&#8226; Delete your account from Piegeon</Text>
+                                </View>
+                                <View style={{marginTop:10}}>
+                                    <Text style={{fontSize:15}}>&#8226; Erase your message history</Text>
+                                </View>
+                                <View style={{marginTop:10}}>
+                                    <Text style={{fontSize:15}}>&#8226; Delete you from all your Piegeon groups</Text>
+                                </View>
+                                <View style={{marginTop:10}}>
+                                    <Text style={{fontSize:15}}>&#8226; Delte your Google Drive backup</Text>
+                                </View>
+                                <View style={{marginTop:10}}>
+                                    <Text style={{fontSize:15}}>&#8226; Delete your payments history and cancel{`\n`}  any pending payments</Text>
+                                </View>
+                            </View>
                         </Body>
 					</ListItem>
-                    <ListItem avatar noBorder button onPress={() =>{navigate('Accounts')}}>
+                    <ListItem avatar noBorder button>
                         <Left>
                             <Button transparent onPress={()=>{navigate('Chat')}}>
-                                <Icon name='verified-user' type="MaterialIcons" style={{fontSize: 28,color:"#075E54"}}/>
+                                <Icon name='sign-out' type="Octicons" style={{fontSize: 28,color:"#075E54"}}/>
                             </Button>
                         </Left>
-                        <Body style={{marginTop:10}}>
-                            <Text style={{fontSize:18}}>Two-step verification</Text>
-                        </Body>
-					</ListItem>
-                    <ListItem avatar noBorder button onPress={() =>{navigate('Accounts')}}>
-                        <Left>
-                            <Button transparent onPress={()=>{navigate('Chat')}}>
-                                <Icon name='exchange' type="FontAwesome" style={{fontSize: 28,color:"#075E54"}}/>
+                        <Body>
+                            <Text style={{fontSize:18}}>Change number instead</Text>
+                            <Button style={{backgroundColor:"#075E54",padding:30,marginTop:10}} onPress={() =>{navigate('Change')}}>
+                                <Text style={{color:"#ffffff"}}>CHANGE NUMBER</Text>
                             </Button>
-                        </Left>
-                        <Body style={{marginTop:10}}>
-                            <Text style={{fontSize:18}}>Change number</Text>
                         </Body>
-					</ListItem>
-                    <ListItem avatar noBorder button onPress={() =>{navigate('Accounts')}}>
-                        <Left>
-                            <Button transparent onPress={()=>{navigate('Chat')}}>
-                                <Icon name='delete-alert' type="MaterialCommunityIcons" style={{fontSize: 28,color:"#075E54"}}/>
-                            </Button>
-                        </Left>
-                        <Body style={{marginTop:10}}>
-                            <Text style={{fontSize:18}}>Delete my account</Text>
+                    </ListItem>
+                    <ListItem avatar noBorder button>
+                        <Body>
+                            <View>
+                                <Text style={{fontSize:16}}>To delete your account, confirm your{`\n`}country code and enter your phone number.{`\n`}</Text>
+                            </View>
+                            <View>
+                                <Item stackedLabel>
+                                    <Icon name='call' type="MaterialIcons" style={{fontSize: 28}}/>
+                                    <Label style={{fontWeight:"bold"}}>Contact</Label>
+                                    <Input placeholder="Contact Number"/>
+                                </Item>
+                            </View>
                         </Body>
-					</ListItem>
+                    </ListItem>
 				</ScrollView>
+                <Fab position="bottomRight" style={{backgroundColor:"red"}} onPress={() =>{navigate('Chat')}}>
+					<Icon name="delete" type="MaterialIcons"/>
+				</Fab>
 			</Container>
 		);
 	}
