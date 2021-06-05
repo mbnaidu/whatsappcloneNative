1d0ebdb458f18fb0c6f46b4681653b576cef8694
mbnaidu <madhucharliehash@gmail.com>
2021-06-05 08:36:56 +0530

    removed variables from settings

--- Settings/Accounts/Verification/Verification.js
+++ Settings/Accounts/Verification/Verification.js
@@ -3,40 +3,6 @@ import { Container, Header, Left, Body, Right, Button, Icon, Title, Thumbnail, F
 import { StatusBar, Text, TextInput,Image, Keyboard, Modal,Pressable, TouchableWithoutFeedback, ScrollView } from 'react-native';
 
 export default class Verification extends Component {
-    constructor(props) {
-		super(props);
-		this.state = { chosenDate: new Date() };
-		this.setDate = this.setDate.bind(this);
-	}
-	state = {
-        selected2: undefined,
-		selected: undefined,
-		selected3: undefined,
-		selected4: undefined,
-	};
-	onValueChange(value) {
-		this.setState({
-		selected: value
-		});
-	}
-    onValueChange2(value) {
-		this.setState({
-		selected2: value
-		});
-	}
-	onValueChange3(value) {
-		this.setState({
-		selected3: value
-		});
-	}
-	onValueChange4(value) {
-		this.setState({
-		selected4: value
-		});
-	}
-    setDate(newDate) {
-		this.setState({ chosenDate: newDate });
-	}
 	render() {
 		StatusBar.setBackgroundColor('#128C7E',true);
 		const { navigate } = this.props.navigation;
