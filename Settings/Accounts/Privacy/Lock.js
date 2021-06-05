608d33dc9bfca77a4a181547f12be23da1c9f8e2
mbnaidu <madhucharliehash@gmail.com>
2021-06-05 14:56:48 +0530

    completed PRIVACY settings contents

--- Settings/Accounts/Privacy/Lock.js
+++ Settings/Accounts/Privacy/Lock.js
@@ -3,6 +3,15 @@ import { Container, Header, Left, Body, Right, Button, Icon, Title, Thumbnail, F
 import { StatusBar, Text, TextInput,Image, Keyboard, Modal,Pressable, TouchableWithoutFeedback, ScrollView } from 'react-native';
 
 export default class Lock extends Component {
+	constructor(props) {
+        super(props);
+        this.state = {
+			isEnabled:false,
+        };
+    }
+    call = () => {
+		this.setState({ isEnabled: !this.state.isEnabled });
+	}
 	render() {
 		StatusBar.setBackgroundColor('#128C7E',true);
 		const { navigate } = this.props.navigation;
@@ -11,7 +20,7 @@ export default class Lock extends Component {
 				<Header style={{backgroundColor:"#075E54",width:"100%"}} button>
 					<Left>
 						<Button transparent onPress={()=>{navigate('Privacy')}}>
-							<Icon name='close' type="MaterialIcons" style={{fontSize: 28}}/>
+							<Icon name='arrow-back' type="MaterialIcons" style={{fontSize: 28}}/>
 						</Button>
 					</Left>
 					<Body>
@@ -19,18 +28,26 @@ export default class Lock extends Component {
                     </Body>
 					<Right>
 						<Button transparent>
-							<Icon name='more-vert' type="MaterialIcons" style={{fontSize: 28}}/>
+							<Icon name='more-vert' type="MaterialIcons" style={{fontSize: 0}}/>
 						</Button>
 					</Right>
 				</Header>
-					<ScrollView>
-                        <ListItem noBorder button>
-                            <Item stackedLabel>
-                                <Icon name='person' type="MaterialIcons" style={{fontSize: 28}}/>
-                                <Label style={{fontWeight:"bold"}}>Contact</Label>
-                                <Input placeholder="Enter contact name"/>
-                            </Item>
-                        </ListItem>
+				<ScrollView>
+					<ListItem avatar noBorder button style={{marginTop:10}}>
+						<Body >
+							<Text style={{fontSize:18,color:"#075E54",marginBottom:5}}>Unlock with fingerprint</Text>
+							<Text note >When enabled, you'll need to use {`\n`} fingerprint to open Piegon. You can still{`\n`}answer calls if Piegeon is locked.</Text>
+						</Body>
+						<Right>
+							<Switch
+								trackColor={{false:'#767577', true:"#128C7E"}}
+								thumbColor={this.state.isEnabled ? '#075E54' : "#767577"}
+								value={this.state.isEnabled}
+								onChange={this.call}
+							>
+							</Switch>
+						</Right>
+					</ListItem>
 				</ScrollView>
 			</Container>
 		);
