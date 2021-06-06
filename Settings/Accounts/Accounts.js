cc1d538db57ace5dd73f12a4fa978ad281d8fd0c
mbnaidu <madhucharliehash@gmail.com>
2021-06-06 09:36:51 +0530

    Update Accounts.js

--- Settings/Accounts/Accounts.js
+++ Settings/Accounts/Accounts.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { Container, Header, Left, Body, Right, Button, Icon, Title, Thumbnail, Footer, Item, Input, Content, Fab, View, ListItem, Badge, Switch, Card, CardItem, Picker, DatePicker, Label } from 'native-base';
-import { StatusBar, Text, TextInput,Image, Keyboard, Modal,Pressable, TouchableWithoutFeedback, ScrollView } from 'react-native';
+import { Container, Header, Left, Body, Right, Button, Icon, Title, ListItem } from 'native-base';
+import { StatusBar, Text, ScrollView } from 'react-native';
 
 export default class Accounts extends Component {
 	render() {
@@ -15,7 +15,7 @@ export default class Accounts extends Component {
 						</Button>
 					</Left>
 					<Body>
-                        <Title>Accounts</Title>
+                        <Title onPress={()=>{navigate('Settings')}}>Accounts</Title>
                     </Body>
 					<Right>
 						<Button transparent>
